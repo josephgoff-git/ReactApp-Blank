d944729fb8d7aedaa005ce831a373a9b7aed5a75
Joseph Goff <98006199+josephgoff-git@users.noreply.github.com>
2023-10-11 14:02:08 -0400

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,12 +1,7 @@
-import logo from './logo.svg';
-import './App.css';
-
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-      </header>
+    <div style={{ backgroundColor: "white", height: "100vh", width: "100vw" }}>
+
     </div>
   );
 }
